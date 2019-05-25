9e722c86c06b8018fb56a090353de1c79823c5c2
Vinicius Dias <carlosv775@gmail.com>
2019-05-25 15:34:18 -0300

    Updates

---
+++ precache-manifest.c36c64d68bfab7e422951c52bc5e3b32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21d04a3aba527050f26716119d42c386",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd74d4aa0dd9293ee8af",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6876771c1b9d69d92bd4",
+    "url": "/static/js/2.4d5948bb.chunk.js"
+  },
+  {
+    "revision": "cd74d4aa0dd9293ee8af",
+    "url": "/static/js/main.b54b683b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
