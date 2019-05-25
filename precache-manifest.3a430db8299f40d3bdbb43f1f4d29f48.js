48bbd6c998e3740c283f7e543daba5c8310217bb
Vinicius Dias <carlosv775@gmail.com>
2019-05-25 15:45:24 -0300

    Updates

---
+++ precache-manifest.3a430db8299f40d3bdbb43f1f4d29f48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a882dc94d39f3aa50c5c50d5ddc5e90",
+    "url": "/arctouch-movies-front/index.html"
+  },
+  {
+    "revision": "db8b57b659bc3d315378",
+    "url": "/arctouch-movies-front/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "fb1c123d925d8aa7e3b0",
+    "url": "/arctouch-movies-front/static/js/2.c4207629.chunk.js"
+  },
+  {
+    "revision": "db8b57b659bc3d315378",
+    "url": "/arctouch-movies-front/static/js/main.720fa586.chunk.js"
+  },
+  {
+    "revision": "4a129826c774536c4d3e",
+    "url": "/arctouch-movies-front/static/js/runtime~main.6347a367.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c36c64d68bfab7e422951c52bc5e3b32.js"
+  "/arctouch-movies-front/precache-manifest.3a430db8299f40d3bdbb43f1f4d29f48.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/arctouch-movies-front/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
